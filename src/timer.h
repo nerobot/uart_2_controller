226eded8c578f406de036447e66778c32b2f70fe
Steven Daglish <s.c.daglish@gmail.com>
2019-10-11 08:27:05 +0100

    Added: Mock timer.h file added

---
+++ src/timer.h
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// mock header file for timer.h
+//
+//  It is assumed that the timer is already set up and initialised.
+//
+//  Only required functions are included as needed.
+
+#ifndef _TIMER_H
+#define _TIMER_H
+
+/**
+ * @brief   Returns the number of milliseconds that have passed since the program 
+ *          has started.
+ * @note    Will roll-over after 49.7 days.
+ */
+uint16_t millis(void);
+
+#endif
+
